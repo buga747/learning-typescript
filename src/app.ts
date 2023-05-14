f45f43fe1893bad1cb3283410beb589c8872a95a
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-05-14 23:49:15 +0300

    interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,193 @@
-class Department {
-  name: string;
+// interface Person {
+//   name: string;
+//   age: number;
 
-  constructor(n: string) {
-    this.name = n;
-  }
-}
+//   greet(phrase: string): void;
+// }
 
-const accounting = new Department("Accounting");
+// let user1: Person;
 
-console.log(accounting);
+// user1 = {
+//   name: "Oleh",
+//   age: 30,
+
+//   greet(phrase: string) {
+//     console.log(phrase + ", " + this.name);
+//   },
+// };
+
+// user1.greet("Hi there");
+
+// =======================================================
+
+// interface Greetable {
+//   name: string;
+
+//   greet(phrase: string): void;
+// }
+
+// class Person implements Greetable {
+//   name: string;
+//   age = 30;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   greet(phrase: string) {
+//     console.log(phrase + ", " + this.name);
+//   }
+// }
+
+// let user1: Greetable;
+
+// user1 = new Person("Oleh");
+
+// user1.greet("Hi there");
+
+// -----------------------------------------------------
+// Read only properties
+
+// interface Greetable {
+//   readonly name: string;
+
+//   greet(phrase: string): void;
+// }
+
+// class Person implements Greetable {
+//   name: string;
+//   age = 30;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   greet(phrase: string) {
+//     console.log(phrase + ", " + this.name);
+//   }
+// }
+
+// let user1: Greetable;
+
+// user1 = new Person("Oleh");
+
+// user1.greet("Hi there");
+
+// ----------------------------------------------
+// interface inheritance
+
+// interface Named {
+//   readonly name: string;
+// }
+
+// interface Greetable extends Named {
+//   greet(phrase: string): void;
+// }
+
+// class Person implements Greetable {
+//   name: string;
+//   age = 30;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   greet(phrase: string) {
+//     console.log(phrase + ", " + this.name);
+//   }
+// }
+
+// let user1: Greetable;
+
+// user1 = new Person("Oleh");
+
+// user1.greet("Hi there");
+
+// ===========================================
+// FUNCTION TYPE INTERFACES
+
+// type AddFn = (n1: number, n2: number) => number;
+
+// interface AddFn {
+//   (n1: number, n2: number): number;
+// }
+
+// let add: AddFn;
+
+// add = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
+
+// interface Named {
+//   readonly name: string;
+// }
+
+// interface Greetable extends Named {
+//   greet(phrase: string): void;
+// }
+
+// class Person implements Greetable {
+//   name: string;
+//   age = 30;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   greet(phrase: string) {
+//     console.log(phrase + ", " + this.name);
+//   }
+// }
+
+// let user1: Greetable;
+
+// user1 = new Person("Oleh");
+
+// user1.greet("Hi there");
+
+// =============================================================
+// OPTIONAL PARAMETRS AND PROPERTIES
+
+// interface AddFn {
+//   (n1: number, n2: number): number;
+// }
+
+// let add: AddFn;
+
+// add = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
+
+// interface Named {
+//   readonly name?: string;
+//   outputName?: string;
+// }
+
+// interface Greetable extends Named {
+//   greet(phrase: string): void;
+// }
+
+// class Person implements Greetable {
+//   name?: string;
+//   age = 30;
+
+//   constructor(name?: string) {
+//     if (name) {
+//       this.name = name;
+//     }
+//   }
+
+//   greet(phrase: string) {
+//     if (this.name) {
+//       console.log(phrase + ", " + this.name);
+//     } else {
+//       console.log("Hello, guest");
+//     }
+//   }
+// }
+
+// let user1: Greetable;
+
+// user1 = new Person();
+
+// user1.greet("Hi there");
