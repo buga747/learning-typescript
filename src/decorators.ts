e9f3acea3b518e73d7be1bec8811524d01c14ce8
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-06-04 12:52:38 +0300

    property decorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -7,6 +7,7 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   return function (constructor: any) {
+    console.log("Rendering template");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -17,6 +18,7 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // @Logger("LOGGING - PERSON")
+@Logger("LOGGING")
 @WithTemplate("<h1>Hello from Template</h1>", "app")
 class Person {
   name = "Oleh";
@@ -29,3 +31,34 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// ==================================================
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator");
+
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
